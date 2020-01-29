906ac7030ce9f18b2e26beab7d6b73aed232f7d3
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-29 18:46:18 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,4 +4,14 @@ class Shoe
     @brand = brand
   end
   
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+  
 end
+
+Shoe.new("Nike")
